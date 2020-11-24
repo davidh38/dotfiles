f72e925d5177944963c4b86549444a04d17a5b1c
lifeainteasy@web.de <lifeainteasy@web.de>
2020-11-24 16:25:45 +0100

    Update config

--- config.el
+++ config.el
@@ -68,7 +68,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-(setq lsp-python-ms-extra-paths ["." ".." "/home/dave/PythonProjects/MyProject/src/" "/home/dave/PythonProjects/MyProject/" "src/"])
+;; (setq lsp-python-ms-extra-paths ["." ".." "/home/dave/PythonProjects/MyProject/src/" "/home/dave/PythonProjects/MyProject/" "src/"])
 
 (setq lsp-python-ms-python-executable-cmd "python3")
 
@@ -77,8 +77,10 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-(setq org-agenda-files '("~/Dropbox/org/"))
+(setq org-directory "~/Dropbox/org/")
+(setq org-agenda-files "~/Dropbox/org/")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 ;;(setq org-image-actual-width 800)
 
 ;; not working
--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("/home/dave/Dropbox/org/notebook.org" "/home/dave/Dropbox/org/schedule.org" "/home/dave/Dropbox/org/birthdays.org" "/home/dave/Dropbox/org/mypdf.org" "/home/dave/Dropbox/org/priv.org" "/home/dave/Dropbox/org/test.org")))
+   '("/home/dave/Dropbox/org/notebook.org" "/home/dave/Dropbox/org/schedule.org" "/home/dave/Dropbox/org/birthdays.org" "/home/dave/Dropbox/org/mypdf.org" "/home/dave/Dropbox/org/priv.org" "/home/dave/Dropbox/org/test.org"))
+ '(package-selected-packages '(request)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
