ea844cd6404fa00634b08de14296d36f1f5705f3
David H <lifeainteasy@web.de>
2022-06-01 21:21:27 +0200

    Update chemacs and init.el

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -2,7 +2,7 @@
 
 ("default" . ((user-emacs-directory . "~/.emacs.d")))
 
- ("coergi" . ((user-emacs-directory . "~/.emacs.coergi")))
+ ("corgi" . ((user-emacs-directory . "~/.emacs.coergi")))
 
 
 ("doom" . ((user-emacs-directory . "~/doomemacs")
